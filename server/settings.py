adadd7e402925a357171dd3f288b98d21713bf77
Mohamed Hamdy <98131964+Adosh74@users.noreply.github.com>
2023-12-17 06:13:40 +0200

    update

--- server/settings.py
+++ server/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-oi%b@p=z_r5jp=$b(r=0^swy3t4%n5ulgiwj9%&vlnm0&uw3^n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', "False").lower() == "true"
+DEBUG = True
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(" ")
+ALLOWED_HOSTS = []
 
 
 # Application definition
